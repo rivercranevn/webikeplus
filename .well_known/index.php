7c6848d5a50f64a1f07db0a89f08fa7693732f13
caosangit <56952057+rivercranevn@users.noreply.github.com>
2021-04-09 13:07:03 +0700

    update

--- .well_known/index.php
+++ .well_known/index.php
@@ -1,6 +1,6 @@
 <?php
 	header('Content-Type: application/json');
-	$strApple = @file_get_contents('https://321c0ba9e4d9.ngrok.io/mobileplus/.well_known/apple-app');	
+	$strApple = @file_get_contents('https://321c0ba9e4d9.ngrok.io/.well_known/apple-app');	
 	echo $strApple; 
 	exit;
 ?>
