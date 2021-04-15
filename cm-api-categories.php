86e2547bab57afd49347cd67fd8b1fb298fe7cc9
caosangit <56952057+rivercranevn@users.noreply.github.com>
2021-04-15 14:38:30 +0700

    Update cm-api-categories.php

--- cm-api-categories.php
+++ cm-api-categories.php
@@ -6,6 +6,7 @@ header("Content-Type: application/json");
 $order_by 		= isset($_GET['order_by'])?$_GET['order_by']:'ASC';
 $post_source 	= isset($_GET['post_source'])?(int)$_GET['post_source']:1;
 $category_id 	= isset($_GET['category_id'])?(int)$_GET['category_id']:null;
+$post_limit 	= isset($_GET['post_limit'])?(int)$_GET['post_limit']:20;
 $app_secret = 'e1111a26fc31aa10f444f69f41f4d5a'; 
 $app_id 	= 'webikeplus-app';				
 $check_sign 	= substr(hash_hmac('sha256', $app_id.'_'.$config['domain'], $app_secret),0,10);
@@ -27,11 +28,18 @@ if($config['key']!=$check_sign)
 		$arrReturn	= array('response_code'=>'S000', 'response_message'=>'OK', 'data'=>array());
 		if(!empty($app_secret)){
 			//?sort=&page=1&per_page=10&search=&config=52
-			$sll = $db->Curl($config['category_api']);
-			//$aContent = $db->getResult($sll); 			
-			//$category_tree = $db->buildTree($aContent,0);
-			print_r($sll); exit; 
-			
+			//$sll = $db->Curl($config['category_api']);
+			//$category_tree = $db->buildTree($aContent,0);						
+			$arrContent = array(); 	
+			$cmDataList = @json_decode($db->Curl($config['category_api']."?config={$post_source}&sort={$order_by}&per_page={$post_limit}&page=1"));	
+			$arrImgs = array(); 
+			foreach($cmDataList->data as $k => $v){				
+				$arrContent[$k]['post_source'] = $v->config_id; 
+				$arrContent[$k]['post_term_id'] = $v->id; 
+				$arrContent[$k]['post_term_name'] = @strip_tags(html_entity_decode($v->title)); 
+				$arrContent[$k]['post_term_parent_id'] = $v->parent_id;  
+			}			
+			$category_tree = $db->buildTree($arrContent,0);
 			$arrReturn	= array('response_code'=>'S000', 'response_message'=>'OK', 'data'=>$category_tree);						
 		}		
 		echo json_encode($arrReturn);	
